1d5c895ab1a46777d3ad762a3ebc8c2176a2998c
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-30 14:57:11 -0400

    add constraints to user table

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -1,10 +1,9 @@
 import sequelize from '../sequelize';
 import User from './User';
-//import UserLogin from './UserLogin';
-//import UserClaim from './UserClaim';
-//import UserProfile from './UserProfile';
+import UserLogin from './UserLogin';
+import UserClaim from './UserClaim';
+import UserProfile from './UserProfile';
 
-/*
 User.hasMany(UserLogin, {
   foreignKey: 'userId',
   as: 'logins',
@@ -25,11 +24,10 @@ User.hasOne(UserProfile, {
   onUpdate: 'cascade',
   onDelete: 'cascade'
 });
-*/
 
 function sync(...args) {
   return sequelize.sync(...args);
 }
 
 export default { sync };
-export { User }; //, UserLogin, UserClaim, UserProfile };
+export { User, UserLogin, UserClaim, UserProfile };
