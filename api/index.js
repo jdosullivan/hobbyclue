94ac46051447edfa9a34648d5e74185a995d19ff
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-31 22:11:57 -0400

    centralized secret

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'react and redux rule!!!!',
+  secret: config.auth.jwt.secret,
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 60000 }
