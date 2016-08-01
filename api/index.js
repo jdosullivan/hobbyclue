3399d84c40ead7053fc3505a5c566b4eb47c8736
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-31 22:04:46 -0400

    move auth stuff to separate file

--- api/index.js
+++ api/index.js
@@ -8,9 +8,7 @@ import PrettyError from 'pretty-error';
 import http from 'http';
 import SocketIo from 'socket.io';
 import sequelizeTables from './database/models';
-import passport from 'passport';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
-import { User, UserClaim } from './database/models';
+import configureAuth from './configureAuth';
 
 const pretty = new PrettyError();
 const app = express();
@@ -26,63 +24,6 @@ app.use(session({
   cookie: {maxAge: 60000}
 }));
 app.use(bodyParser.json());
-
-// Authentication
-passport.use(new FacebookStrategy({
-    clientID: config.auth.facebook.id,
-    clientSecret: config.auth.facebook.secret,
-    callbackURL: 'http://localhost:3000/api/login/facebook/return',
-    passReqToCallback : true,
-    profileFields: ['id', 'emails', 'name', 'displayName', 'gender']
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    const loginName = 'facebook';
-    const claimType = 'urn:facebook:access_token';
-
-    const findOrCreateFBUser = async () => {
-      if (profile) {
-
-        // Look up user by profile id
-        let user = await User.findOne({
-          where: { profileType: loginName, profileId: profile.id }
-        });
-
-        // Create a new user in the user table if not found
-        if (!user) {
-            user = await User.create({
-            name: profile.displayName,
-            email: profile._json.email,
-            profileId: profile.id,
-            profileType: loginName,
-            gender: profile._json.gender,
-            picture: `https://graph.facebook.com/${profile.id}/picture?type=large`,
-            claims: [
-              { type: claimType, value: profile.id }
-            ]
-          }, {
-            include: [
-              { model: UserClaim, as: 'claims' }
-            ]
-          });
-        }
-
-        // Return the user
-        done(null, user);
-      }
-    };
-
-    findOrCreateFBUser().catch(done);
-  }
-));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   secret: 'react and redux rule!!!!',
@@ -91,19 +32,7 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/login/facebook', passport.authenticate('facebook'));
-
-app.get('/login/facebook/return',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log(`fb success with user ${JSON.stringify(req.user)}`);
-    res.redirect('http://localhost:3000');
-  }
-);
-
+configureAuth(app, config);
 
 app.use((req, res) => {
   const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
