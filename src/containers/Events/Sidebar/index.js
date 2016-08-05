8af59f7deea1be19bc66fb8fb28721e2963d122f
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-05 11:55:00 -0400

    baseline it

--- src/containers/Events/Sidebar/index.js
+++ src/containers/Events/Sidebar/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 
 const Sidebar = () => {
-  const styles = require( './Sidebar.scss' );
 
   return (
-    <div className={styles.sidebar}>
-      <div className={styles.block}>
-        <h3>Categories</h3>
-        <div className={styles.divline}></div>
-        <div className={styles.blocktxt}>
-          <ul className={styles.cats}>
-            <li><a href="#">Trading for Money <span className="badge pull-right">20</span></a></li>
-            <li><a href="#">Vault Keys Giveway <span className="badge pull-right">10</span></a></li>
-            <li><a href="#">Misc Guns Locations <span className="badge pull-right">50</span></a></li>
-            <li><a href="#">Looking for Players <span className="badge pull-right">36</span></a></li>
-            <li><a href="#">Stupid Bugs &amp; Solves <span className="badge pull-right">41</span></a></li>
-            <li><a href="#">Video &amp; Audio Drivers <span className="badge pull-right">11</span></a></li>
-            <li><a href="#">2K Official Forums <span className="badge pull-right">5</span></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div >Sidebar</div>
   );
 };
-Sidebar.propTypes = {};
-
 export default Sidebar;
