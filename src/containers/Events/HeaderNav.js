2e3b8d110d497ef81b70ebc1dc42c4d16d8197c3
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-05 00:18:16 -0400

    Split out and style the searchBox

--- src/containers/Events/HeaderNav.js
+++ src/containers/Events/HeaderNav.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Avatar from './Avatar';
 
+
+const styles = require( './HeaderNav.scss' );
+
+const SearchBox = () => {
+  return (
+    <div className={styles.search}>
+      <div className={styles.wrap}>
+        <form action="#" method="post" className="form">
+          <div className={styles.txt + ' pull-left'}>
+            <input type="text" className="form-control" placeholder="Search Topics"/>
+          </div>
+          <div className="pull-right">
+            <button className="btn btn-default" type="button">
+              <i className="fa fa-search"/>
+            </button>
+          </div>
+          <div className="clearfix"></div>
+        </form>
+      </div>
+    </div>);
+};
+
 function HeaderNav() {
   const logo = require( './images/logo.jpg' );
-  const styles = require( './HeaderNav.scss' );
 
   return (
-    <div className={styles.headernav}>
+    <div className={styles.headerNav}>
       <div className="container">
         <div className="row">
           <div className="col-lg-1 col-xs-3 col-sm-2 col-md-2 logo">
@@ -14,20 +35,8 @@ function HeaderNav() {
               <img src={logo} alt=""/>
             </a>
           </div>
-          <div className={styles.search + ' col-lg-4 hidden-xs hidden-sm col-md-3'}>
-            <div className={styles.wrap}>
-              <form action="#" method="post" className="form">
-                <div className={styles.txt + ' pull-left'}>
-                  <input type="text" className="form-control" placeholder="Search Topics"/>
-                </div>
-                <div className="pull-right">
-                  <button className="btn btn-default" type="button">
-                    <i className="fa fa-search"/>
-                  </button>
-                </div>
-                <div className="clearfix"></div>
-              </form>
-            </div>
+          <div className="col-lg-4 hidden-xs hidden-sm col-md-3">
+            <SearchBox />
           </div>
           <div className={styles.avt + ' col-lg-4 col-xs-12 col-sm-5 col-md-4'}>
             <div className="pull-left">
