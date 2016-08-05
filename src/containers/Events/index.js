b2d0a45220b6d11a4d04773bf4e4008d61d7dd84
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-05 01:43:44 -0400

    No sidebar or headerNav still troubleshooting

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
-import Sidebar from './Sidebar';
-import HeaderNav from './HeaderNav';
 
 export default class Events extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <HeaderNav />
         <section className="pageContent">
           <div className="container">
             <div className="row">
               <div className="col-lg-8 col-md-8">
+                Events page
               </div>
               <div className="col-lg-4 col-md-4">
-                <Sidebar />
               </div>
             </div>
           </div>
