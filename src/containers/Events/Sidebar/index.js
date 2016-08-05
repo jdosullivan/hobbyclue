f327885e1ebc2b02a939690c2e8bec16d474493a
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-05 12:06:43 -0400

    Test styles

--- src/containers/Events/Sidebar/index.js
+++ src/containers/Events/Sidebar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Sidebar = () => {
+  const styles = require( './Sidebar.scss' );
 
   return (
-    <div>
-      <div>
+    <div className={styles.sidebar}>
+      <div className={styles.block}>
         <h3>Categories</h3>
       </div>
     </div>
