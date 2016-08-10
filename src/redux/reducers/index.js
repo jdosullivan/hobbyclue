ef9283055e02dbf6ba384362f8a0eb9ab01093ab
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-10 15:01:09 -0400

    move reducers into folder

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,15 +3,15 @@ import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
-import auth from './reducers/authReducer';
-import counter from './reducers/counterReducer';
-import filters from './reducers/filtersReducer';
+import auth from 'index.js/authReducer';
+import counter from 'index.js/counterReducer';
+import filters from 'index.js/filtersReducer';
 import {reducer as form} from 'redux-form';
-import info from './reducers/infoReducer';
-import widgets from './reducers/widgetsReducer';
-import groups from './reducers/groupsReducer';
-import posts from './reducers/postsReducer';
-import city from './reducers/cityReducer';
+import info from 'index.js/infoReducer';
+import widgets from 'index.js/widgetsReducer';
+import groups from 'index.js/groupsReducer';
+import posts from 'index.js/postsReducer';
+import city from 'index.js/cityReducer';
 
 export default combineReducers({
   routing: routerReducer,
