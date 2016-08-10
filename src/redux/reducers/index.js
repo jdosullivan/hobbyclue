f0609f1e3c5c8838ac7e9428a7684a4e035a8846
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-10 15:06:06 -0400

    bug with refactoring

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,16 +2,16 @@ import { combineReducers } from 'redux';
 import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
-
-import auth from 'index.js/authReducer';
-import counter from 'index.js/counterReducer';
-import filters from 'index.js/filtersReducer';
 import {reducer as form} from 'redux-form';
-import info from 'index.js/infoReducer';
-import widgets from 'index.js/widgetsReducer';
-import groups from 'index.js/groupsReducer';
-import posts from 'index.js/postsReducer';
-import city from 'index.js/cityReducer';
+
+import auth from './authReducer';
+import counter from './counterReducer';
+import filters from './filtersReducer';
+import info from './infoReducer';
+import widgets from './widgetsReducer';
+import groups from './groupsReducer';
+import posts from './postsReducer';
+import city from './cityReducer';
 
 export default combineReducers({
   routing: routerReducer,
