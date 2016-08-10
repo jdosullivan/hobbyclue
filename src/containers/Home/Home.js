adaf00bbe9160be13f690ffdb0c6d5387ada80bc
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-10 18:29:39 -0400

    changed logo path

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
   render() {
     const styles = require('./Home.scss');
     // require the logo image both from client and server
-    const logoImage = require('./logo.png');
+    const logoImage = require('../App/logo.png');
     return (
       <div className={styles.home}>
         <Helmet title="Home"/>
