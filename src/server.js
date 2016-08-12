7e227747e1b62d474c0af1501778c8337afd3bff
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-11 22:03:29 -0400

    Use the newsletter at the top of the page

--- src/server.js
+++ src/server.js
@@ -84,36 +84,37 @@ app.use((req, res) => {
     hydrateOnClient();
     return;
   }
+  if (req.originalUrl === '/') {
+    return landingPg(res);
+  } else if (!req.originalUrl.startsWith('/landing/')) {
+    match( {history, routes: getRoutes( store ), location: req.originalUrl}, (error, redirectLocation, renderProps) => {
+      if (redirectLocation) {
+        res.redirect( redirectLocation.pathname + redirectLocation.search );
+      } else if (error) {
+        console.error( 'ROUTER ERROR:', pretty.render( error ) );
+        res.status( 500 );
+        hydrateOnClient();
+      } else if (renderProps) {
+        loadOnServer( {...renderProps, store, helpers: {apiClient}} ).then( () => {
+          const component = (
+            <Provider store={store} key="provider">
+              <ReduxAsyncConnect {...renderProps} />
+            </Provider>
+          );
 
-  match({history, routes: getRoutes(store), location: req.originalUrl}, (error, redirectLocation, renderProps) => {
-    if (redirectLocation) {
-      res.redirect(redirectLocation.pathname + redirectLocation.search);
-    } else if (error) {
-      console.error('ROUTER ERROR:', pretty.render(error));
-      res.status(500);
-      hydrateOnClient();
-    } else if (req.originalUrl === '/') {
-      return landingPg(res);
-    } else if (renderProps) {
-      loadOnServer({...renderProps, store, helpers: {apiClient}}).then(() => {
-        const component = (
-          <Provider store={store} key="provider">
-            <ReduxAsyncConnect {...renderProps} />
-          </Provider>
-        );
+          res.status( 200 );
 
-        res.status(200);
+          global.navigator = {userAgent: req.headers['user-agent']};
 
-        global.navigator = {userAgent: req.headers['user-agent']};
-
-        res.send('<!doctype html>\n' +
-          ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component}
-                                        store={store}/>));
-      });
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
+          res.send( '<!doctype html>\n' +
+            ReactDOM.renderToString( <Html assets={webpackIsomorphicTools.assets()} component={component}
+                                           store={store}/> ) );
+        } );
+      } else {
+        res.status( 404 ).send( 'Not found' );
+      }
+    } );
+  }
 });
 
 
