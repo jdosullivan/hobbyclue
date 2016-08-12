de19b0c6760674b78e64db741dc69d628bfcd4e9
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-12 00:45:33 -0400

    cleanup config

--- config.js
+++ config.js
@@ -39,8 +39,8 @@ module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
   apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIPORT,
-  databaseUrl: process.env.DATABASE_URL || 'postgres://jahmaiosullivan:Star2016@localhost:5432/HobbyClue',
+  apiPort: process.env.APIPORT || 3030,
+  databaseUrl: process.env.DATABASE_URL,
   tagLine,
   auth,
   app: {
