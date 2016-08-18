984b08f599f99ac9a19f076f91d2160aa859c6e8
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-18 00:11:54 -0400

    remove comments

--- api/actions/file/save.js
+++ api/actions/file/save.js
@@ -31,54 +31,6 @@ export default function save(req, res, nxt) {
           }
         });
       } );
-
-      /* file.on('data', function(data) {
-
-       });
-
-       file.on('end', function() {
-       blobService.createBlockBlobFromStream( 'feedcontainer', filename, file, 1024 * 50, function (error, result, response) {
-       if (error) {
-       console.log( `createBlockBlobFromLocalFile error is 33 ${util.inspect( error )}` );
-       }
-       if (!error) {
-       console.log( `createBlockBlobFromLocalFile response is 44 ${util.inspect( response )}` );
-       // file uploaded
-       }
-       });
-       });*/
-
-      /*      file.on('end', function() {
-       console.log(`file end with bytes: ${byteCount}`);
-       if (byteCount === 0) {
-       console.log(`Error in api/file/save: Bytecount of ${filename} is 0 bytes. Skipping upload`);
-       }
-       else {*/
-      /*
-       blobService.createContainerIfNotExists('feedcontainer', { publicAccessLevel: 'blob' }, function(error, result, response) {
-       if(error) {
-       console.log( `createContainerIfNotExists error is ${util.inspect( error )}` );
-       }
-       if (!error) {
-       // if result = true, container was created.
-       // if result = false, container already existed.
-       console.log(`uploading file 2 with byteCount ${byteCount}`);
-       blobService.createBlockBlobFromStream('feedcontainer', filename, file, 1024 * 50, function(error, result, response) {
-       console.log(`createBlockBlobFromLocalFile error is ${util.inspect(error)}`);
-       console.log(`createBlockBlobFromLocalFile result is ${util.inspect(result)}`);
-       console.log(`createBlockBlobFromLocalFile response is ${util.inspect(response)}`);
-       if (!error) {
-       // file uploaded
-       }
-       });
-       }
-       });*/
-
-
-      /*console.log( 'File [' + fieldname + '] Finished with bytecount ' + byteCount );
-       }
-       });*/
-
     } );
     req.busboy.on( 'field', function (key, value, keyTruncated, valueTruncated) {
       // ...
