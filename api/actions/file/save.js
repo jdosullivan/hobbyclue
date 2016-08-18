117f7cce6e12200b74e134e9b0558a4bf5dfa8f7
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-18 17:57:14 -0400

    refactor api/file/save method

--- api/actions/file/save.js
+++ api/actions/file/save.js
@@ -2,40 +2,49 @@ import azure from 'azure-storage';
 import path from 'path';
 import fs from 'fs';
 
-export default function save(req) {
+const uploadsDir = path.join( __dirname, '../../..', 'static', 'uploads' );
+const uploadWriteStreamToAzure = (savedFileName, tmpFilePath, reject, resolve) => {
+  const containerName = 'postimages';
+  const hostName = 'https://hobbyclueprod1.blob.core.windows.net';
   const blobService = azure.createBlobService();
 
-  return new Promise((resolve, reject) => {
+  blobService.createContainerIfNotExists( containerName, {publicAccessLevel: 'blob'}, function (error) {
+    if (error) {
+      reject( error );
+    }
+    else {
+      blobService.createBlockBlobFromLocalFile( containerName, savedFileName, tmpFilePath, function (error, result, response) {
+        if (error) {
+          reject( error );
+        }
+        else {
+          const uploadedUrl = blobService.getUrl( containerName, savedFileName, null, hostName );
+          resolve( {response, result, url: uploadedUrl} );
+        }
+      });
+    }
+  });
+};
+const saveTempFile = (file, tmpFilePath) => {
+  var ws = fs.createWriteStream( tmpFilePath );
+  file.pipe( ws );
+  return ws;
+};
+
+
+export default function save(req) {
+  return new Promise( (resolve, reject) => {
     if (req.busboy) {
       req.busboy.on( 'file', function (fieldname, file) {
         const savedFileName = path.basename( fieldname );
-        var tmpFilePath = path.join( __dirname, '../../..', 'static', 'uploads', savedFileName );
-        var ws = fs.createWriteStream( tmpFilePath );
-        file.pipe( ws );
+        const tmpFilePath = path.join( uploadsDir, savedFileName );
 
-        ws.on( 'finish', function () {
-          const containerName = 'postimages';
-          const hostName = 'https://hobbyclueprod1.blob.core.windows.net';
-          blobService.createContainerIfNotExists( containerName, {publicAccessLevel: 'blob'}, function (error) {
-            if (error) {
-              reject( error );
-            }
-            else {
-              blobService.createBlockBlobFromLocalFile( containerName, savedFileName, tmpFilePath, function (error, result, response) {
-                if (error) {
-                  reject( error );
-                }
-                else {
-                  const uploadedUrl = blobService.getUrl(containerName, savedFileName, null, hostName);
-                  resolve( { response, result, url: uploadedUrl });
-                }
-              });
-            }
-          });
+        saveTempFile(file, tmpFilePath, savedFileName).on( 'finish', () => {
+          uploadWriteStreamToAzure( savedFileName, tmpFilePath, reject, resolve);
         });
       });
-
       req.pipe( req.busboy );
     }
   });
 }
+
